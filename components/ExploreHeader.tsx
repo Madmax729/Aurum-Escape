d427a08555c9d05d67280d8f173004994ffbe08d
Madmax729 <sujalpathrabe@gmail.com>
2025-03-03 22:10:24 +0530

    search bar

--- components/ExploreHeader.tsx
+++ components/ExploreHeader.tsx
@@ -3,7 +3,7 @@ import { useRef, useState } from 'react';
 import Colors from '@/constants/Colors';
 import { Ionicons } from '@expo/vector-icons';
 import { MaterialIcons } from '@expo/vector-icons';
-import * as Haptics from 'expo-haptics';
+// import * as Haptics from 'expo-haptics';
 import { Link } from 'expo-router';
 
 const categories = [
@@ -43,34 +43,79 @@ const categories = [
 
 const ExploreHeader = () => {
   return (
-    <SafeAreaView style = {{ flex: 1 , backgroundColor: 'white'}}>
+    <SafeAreaView style = {{ flex: 1 , backgroundColor: '#fff'}}>
     <View style={styles.container}>
-      <View style = {styles.actionRow} >
-        <Link href = "/(modals)/booking"> Booking </Link>
+      <View style = {styles.actionRow}  >
+        <Link href = "/(modals)/booking" asChild>
+          <TouchableOpacity style = {styles.searchBtn}>
+            <Ionicons name='search' size={24}  />
+           <View>
+           <Text style = {{fontFamily: 'mon-sb'}}> Where to? </Text>
+            <Text style = {{fontFamily: 'mon', color: Colors.grey}}>Anywhere · Any week</Text>
+           </View>
 
-        <TouchableOpacity>
-   <Text> Filter </Text>
+          
           </TouchableOpacity>
+          
+           </Link>
 
-
-
-
+        <TouchableOpacity style= {styles.filterBtn}>
+   <Ionicons name='options-outline' size={24}/>
+          </TouchableOpacity>
       </View>
+
+<ScrollView horizontal>
+    {categories.map((category, index) => (}
+
+
+</ScrollView>
+
     </View>
     </SafeAreaView>
   )
 }
 const styles = StyleSheet.create({
   container:{
-    backgroundColor: 'red',
+    backgroundColor: '#fff',
     height: 130,
   },
   actionRow:{
-    flexDirection: 'row',  
+    flexDirection: 'row', 
+    alignItems: 'center', 
+    justifyContent: 'space-between',
+    paddingHorizontal: 24,
+    paddingBottom: 16,
+    gap: 10,
 },
 filterBtn: {
-    
+    padding: 10,
+    borderWidth: 1,
+    borderColor: Colors.grey,
+    borderRadius: 24,
+},
+searchBtn: {
+  flexDirection: 'row',
+  alignItems: 'center',
+  gap:10 ,
+  borderColor: '#c2c2c2',
+  borderWidth: StyleSheet.hairlineWidth,
+  flex: 1,
+  padding: 14,
+  borderRadius: 30,
+  backgroundColor: '#fff',
+
+  elevation: 2,
+  shadowColor: '#000',
+  shadowOpacity: 0.12,
+  shadowRadius: 8,
+  shadowOffset: {
+    width: 1,
+    height: 1,
+  },
+
+
 }
+
 
 })
 
