79ca39051ad0efe62d65e8ff1c206a0ba210d69b
Madmax729 <sujalpathrabe@gmail.com>
2025-03-04 22:18:14 +0530

    test

--- components/ExploreHeader.tsx
+++ components/ExploreHeader.tsx
@@ -1,10 +1,12 @@
-import { View, Text, SafeAreaView, StyleSheet, ScrollView, TouchableOpacity } from 'react-native';
+import { View, Text, SafeAreaView, StyleSheet, ScrollView, TouchableOpacity, Touchable } from 'react-native';
 import { useRef, useState } from 'react';
 import Colors from '@/constants/Colors';
 import { Ionicons } from '@expo/vector-icons';
 import { MaterialIcons } from '@expo/vector-icons';
-// import * as Haptics from 'expo-haptics';
+import * as Haptics from 'expo-haptics';
 import { Link } from 'expo-router';
+// import { Touchable } from 'react-native';
+
 
 const categories = [
     {
@@ -41,7 +43,27 @@ const categories = [
     onCategoryChanged: (category: string) => void;
   }
 
-const ExploreHeader = () => {
+const ExploreHeader = ({ onCategoryChanged }: Props) => {
+  const scrollRef = useRef<ScrollView>(null);
+  const itemRef = useRef<Array<TouchableOpacity | null>>([]);
+  const [activeIndex, setActiveIndex] = useState(1);
+
+  const selectCategory = (index: number) => {
+    const selected = itemRef.current[index];
+  setActiveIndex(index);
+
+  selected?.measure((x) => {
+  scrollRef.current?.scrollTo({
+    x: x - 16,
+    y: 0,
+    animated: true,
+  });
+})
+
+Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light);
+
+  }
+
   return (
     <SafeAreaView style = {{ flex: 1 , backgroundColor: '#fff'}}>
     <View style={styles.container}>
@@ -64,60 +86,107 @@ const ExploreHeader = () => {
           </TouchableOpacity>
       </View>
 
-<ScrollView horizontal>
-    {categories.map((category, index) => (}
+<ScrollView
+ref={scrollRef}
+horizontal showsHorizontalScrollIndicator={false}
+contentContainerStyle ={{
+  alignItems: 'center',
+  gap: 30,
+  paddingHorizontal: 16,
+}}
 
+>
+    {categories.map((item, index) => (
+    <TouchableOpacity key={index}
+    ref = {(el) => itemRef.current[index] = el}
+    style = {activeIndex === index ?  styles.categoriesBtnActive : styles.categoriesBtn}
+    onPress={() => selectCategory(index)}
+    >
+      <MaterialIcons name={item.icon as any} size={24}
+      color={activeIndex === index ? '#000' : Colors.grey} />
+      <Text style={ activeIndex === index ? styles.categoryTextActive : styles.categoryText}> {item.name}</Text>
+    </TouchableOpacity>
 
+    
+    ))}
 </ScrollView>
 
     </View>
     </SafeAreaView>
   )
 }
+
 const styles = StyleSheet.create({
-  container:{
+  container: {
     backgroundColor: '#fff',
     height: 130,
+    elevation: 2,
+    shadowColor: '#000',
+    shadowOpacity: 0.2,
+    shadowRadius: 6,
+    shadowOffset: {
+      width: 1,
+      height: 10,
+    },
   },
-  actionRow:{
-    flexDirection: 'row', 
-    alignItems: 'center', 
+  actionRow: {
+    flexDirection: 'row',
+    alignItems: 'center',
     justifyContent: 'space-between',
     paddingHorizontal: 24,
     paddingBottom: 16,
-    gap: 10,
-},
-filterBtn: {
-    padding: 10,
-    borderWidth: 1,
-    borderColor: Colors.grey,
-    borderRadius: 24,
-},
-searchBtn: {
-  flexDirection: 'row',
-  alignItems: 'center',
-  gap:10 ,
-  borderColor: '#c2c2c2',
-  borderWidth: StyleSheet.hairlineWidth,
-  flex: 1,
-  padding: 14,
-  borderRadius: 30,
-  backgroundColor: '#fff',
-
-  elevation: 2,
-  shadowColor: '#000',
-  shadowOpacity: 0.12,
-  shadowRadius: 8,
-  shadowOffset: {
-    width: 1,
-    height: 1,
   },
 
-
-}
-
-
-})
+  searchBtn: {
+    backgroundColor: '#fff',
+    flexDirection: 'row',
+    gap: 10,
+    padding: 14,
+    alignItems: 'center',
+    width: 280,
+    borderWidth: StyleSheet.hairlineWidth,
+    borderColor: '#c2c2c2',
+    borderRadius: 30,
+    elevation: 2,
+    shadowColor: '#000',
+    shadowOpacity: 0.12,
+    shadowRadius: 8,
+    shadowOffset: {
+      width: 1,
+      height: 1,
+    },
+  },
+  filterBtn: {
+    padding: 10,
+    borderWidth: 1,
+    borderColor: '#A2A0A2',
+    borderRadius: 24,
+  },
+  categoryText: {
+    fontSize: 14,
+    fontFamily: 'mon-sb',
+    color: Colors.grey,
+  },
+  categoryTextActive: {
+    fontSize: 14,
+    fontFamily: 'mon-sb',
+    color: '#000',
+  },
+  categoriesBtn: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingBottom: 8,
+  },
+  categoriesBtnActive: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderBottomColor: '#000',
+    borderBottomWidth: 2,
+    paddingBottom: 8,
+  },
+});
 
 
 export default ExploreHeader
